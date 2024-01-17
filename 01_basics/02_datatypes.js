7683935295bc04493fbf4578c11724b8a6fe77a0
Abdullah Sunasara <148894144+Abdullah00110@users.noreply.github.com>
2024-01-17 18:33:00 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all javascript code as a newer version
+
+// alert("Abd") // We are using nodejs not browser.
+
+
+console.log(1
+    +              // code readability should high
+    1)
+
+
+let name = "Abdullah" // ----> string datatype
+let age = 21 // ---> number datatype
+let isLoggedIn = true // --> Bool datatype
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // => undefined
+console.log(typeof null) // => object
