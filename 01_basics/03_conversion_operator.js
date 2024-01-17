af908ef8ad5374e1482667bac48aab35f068c008
Abdullah Sunasara <148894144+Abdullah00110@users.noreply.github.com>
2024-01-17 18:33:59 +0000

    Conversion operator

---
+++ 01_basics/03_conversion_operator.js
@@ -0,0 +1,54 @@
+// let score = 54;
+
+// let score1 = "54"
+// console.log(typeof score);
+// console.log(typeof score1)
+
+// let stringToNumber = Number (score1)
+
+// console.log(typeof stringToNumber);
+
+
+
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1 ; false => 0
+
+
+// let isLoggedIn = 1
+// console.log(typeof Boolean(isLoggedIn));
+
+
+// // 1 => true ; 0 => false
+// // "" => false
+// // "Abdullah" => true
+
+
+// let number = 33
+
+// let numberToString = String(number)
+
+// console.log(typeof numberToString);
+
+
+
+// **************************************Operation*******************************************
+
+// let a = 4
+// let b = 5
+// console.log(a + b);
+
+
+// let a = true
+// let b = false
+
+// console.log(a + b);
+
+
+
+// let gameCounter = 5
+// gameCounter++
+// console.log(`Output : ${gameCounter}`);
+
+
+// console.log(1 * 2 + "3")
