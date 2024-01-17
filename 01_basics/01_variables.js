026d1a3954aca6874807f576ad80039789e6f444
Abdullah Sunasara <148894144+Abdullah00110@users.noreply.github.com>
2024-01-17 18:31:43 +0000

    let , var , const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 111;
+let accountEmail = "abd123@example.com";
+var accountPassword = "123";
+accountCity = "Mumbai";
+
+// accountId = 124;
+accountEmail = "abd111@example.com"
+console.log(accountId)
+console.log(accountEmail)
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
