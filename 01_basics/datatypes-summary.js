0a046232cb6be3f8c7831c779d1f11ddd275dc24
Abdullah Sunasara <148894144+Abdullah00110@users.noreply.github.com>
2024-01-18 14:50:47 +0000

    Datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// Primitive datatype  => Call by value
+
+// Javascript is dynamically typed language
+
+// 7 types
+
+// null means empty 
+
+// String , Number , Boolean , null , undefined , Symbol, BigInt
+
+
+const score = 100
+
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+// let userEmail = undefined ; let userEmail1 ; => Both are same for undefined
+
+const id = Symbol("123");
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+// Refrence (Non Primitive)
+
+// Array , object , Function
+
+let batsman = ["Rohit" , "Virat" , "Sachin" , "Dhoni"]
+
+let myobj = {
+
+    name : 'Abd',
+    age : 21,
+    roll : 761
+}
+
+const abc = function(){
+    
+}
