58099dde4f6b3560d705cf37d0ac5564f49e2394
Abdullah Sunasara <148894144+Abdullah00110@users.noreply.github.com>
2024-01-24 18:28:11 +0000

    Array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -3,5 +3,17 @@
 const myarray = [0,1,2,3]
 // myarray.pop()
 
-console.log(myarray.indexOf(3))
+// console.log(myarray.indexOf(3))
 // console.log(myarray);
+
+
+//slice
+
+const myarray1 = [0,1,2,3,4,5]
+console.log("A ---> " , myarray1)
+
+const myn1 = myarray1.slice(1,3)
+console.log(myn1);
+
+const myn2 = myarray1.splice(1,4)
+console.log(myn2);
