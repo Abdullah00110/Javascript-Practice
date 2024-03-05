53c87ae0fc387684f857f078a12c9fbba70fbe00
Abdullah Sunasara <148894144+Abdullah00110@users.noreply.github.com>
2024-03-05 18:13:31 +0000

    Function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,19 +1,20 @@
-function sayMyName(){
-    console.log("A")
-    console.log("B")
-    console.log("D")
-    console.log("U")
-    console.log("L")
-    console.log("L")
-    console.log("A")
-    console.log("H")
-}
+// function sayMyName(){
+//     console.log("A")
+//     console.log("B")
+//     console.log("D")
+//     console.log("U")
+//     console.log("L")
+//     console.log("L")
+//     console.log("A")
+//     console.log("H")
+// }
 
-sayMyName()
+// sayMyName()
 
 function sayHello(a, b){
-    // console.log("Abd")
     return a + b;
 }
 
-sayHello(3,7)
+const result = sayHello(3,7)
+
+console.log(result);
